97ecd3d73f38160c74b43ea0aba9ea6254e80c46
Lucas Vinícius <62507586+lucasviinic@users.noreply.github.com>
2022-04-16 16:37:54 -0300

    Componentizando widgets

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,36 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'myapp.dart';
+
 void main() {
   runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomePage()
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override 
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-      body: Center(
-        child: Text('Flutterandoo'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          print('Clicou');
-        },
-      ),
-    );
-  }
 }
