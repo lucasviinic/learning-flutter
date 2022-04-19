d39ddabcfd9d62d4243825dc6e7431581aede6cb
Lucas Vinícius <62507586+lucasviinic@users.noreply.github.com>
2022-04-19 10:14:43 -0300

    O StatefulWidget

--- hello_world/lib/home_page.dart
+++ hello_world/lib/home_page.dart
@@ -1,29 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:hello_world/home_controller.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({ Key? key }) : super(key: key);
 
-  @override 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  var counter = 0;
+
+  @override
   Widget build(BuildContext context) {
-    var controller = HomeController.of(context);
-    final size = MediaQuery.of(context).size;
-    final navigation = Navigator.of(context);
-    final theme = Theme.of(context);
-
     return Scaffold(
         appBar: AppBar(
           title: Text('Home'),
         ),
         body: Center(
-          child: Text('Flutterando ${controller.value}'),
+          child: Text('Flutterando $counter'),
         ),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: () {
-            controller.increment();
+            setState(() {
+              counter++;
+            });
           },
         ),
-      );
+    );
   }
 }
