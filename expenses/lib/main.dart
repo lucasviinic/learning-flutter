6e57a3bc5378ab17f5ba3f99f23cd23e8810808b
lucasviinic <lvms@cin.ufpe.br>
2022-05-21 14:27:52 -0300

    criando classe de modelo para transações

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
 
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main () => runApp(ExpensesApp());
@@ -15,14 +16,41 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now()
+    ),
+    Transaction(
+      id: 't2', 
+      title: 'Conta de Luz', 
+      value: 311.30, 
+      date: DateTime.now()
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Despesas pessoais"),
       ),
-      body: Center(
-        child: Text("Versão inicial")
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Text("Gráfico"),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text("Lista de Transações"),
+          )
+        ]
       ),
     );
   }
