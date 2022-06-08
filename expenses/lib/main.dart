53fea13ece10d70f67d236996533fe1ab0b124da
lucasviinic <lvms@cin.ufpe.br>
2022-06-07 22:42:40 -0300

    feat: fechando modal após adicionar item a lista

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -59,9 +59,12 @@ class _MyHomePageState extends State<MyHomePage> {
       value: value, 
       date: DateTime.now()
     );
+
     setState(() {
       _transactions.add(newTransaction);
     });
+
+    Navigator.of(context).pop();
   }
 
   @override
