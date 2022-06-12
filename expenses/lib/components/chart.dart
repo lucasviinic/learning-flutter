009f7080b4dc1d8ba08794cfdba420bfe328f3c8
lucasviinic <lvms@cin.ufpe.br>
2022-06-11 21:33:42 -0300

    feat: definindo widget do gráfico

---
+++ expenses/lib/components/chart.dart
@@ -0,0 +1,48 @@
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
+
+import 'package:expenses/models/transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class Chart extends StatelessWidget {
+
+  final List<Transaction> recentTransaction;
+  Chart(this.recentTransaction);
+  
+  List<Map<String, Object>> get groupedTransactions {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index)
+      );
+
+      double totalSum = 0.0;
+
+      for(int i = 0; i < recentTransaction.length; i++) {
+        bool sameDay = recentTransaction[i].date.day == weekDay.day;
+        bool sameMonth = recentTransaction[i].date.month == weekDay.month;
+        bool sameYear = recentTransaction[i].date.year == weekDay.year;
+
+        if(sameDay && sameMonth && sameYear) {
+          totalSum += recentTransaction[i].value;
+        }
+      }
+
+      print("Day: ${DateFormat.E().format(weekDay)[0]}");
+      print("Value: $totalSum");
+
+      return {
+        'day': DateFormat.E().format(weekDay)[0], 
+        'value': totalSum
+      };
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(children: []),
+    );
+  }
+}
