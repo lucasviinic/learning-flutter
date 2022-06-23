dce13f6ced15f2ea78b4741a921bab959b36a348
lucasviinic <lvms@cin.ufpe.br>
2022-06-22 21:44:03 -0300

    feat: permitindo escolha de data para cada transação

--- expenses/lib/components/chart.dart
+++ expenses/lib/components/chart.dart
@@ -56,7 +56,7 @@ class Chart extends StatelessWidget {
               child: ChartBar(
                 label: tr['day'],
                 value: tr['value'],
-                percentage: tr['value'] / _weekTotalValue,
+                percentage: _weekTotalValue == 0 ? 0 : tr['value'] / _weekTotalValue,
               ),
             );
           }).toList(),
--- expenses/lib/components/transaction_form.dart
+++ expenses/lib/components/transaction_form.dart
@@ -5,7 +5,7 @@ import 'package:intl/intl.dart';
 
 class TransactionForm extends StatefulWidget {
 
-  final void Function(String, double)? onSubmit;
+  final void Function(String, double, DateTime)? onSubmit;
   TransactionForm(this.onSubmit);
 
   @override
@@ -15,17 +15,17 @@ class TransactionForm extends StatefulWidget {
 class _TransactionFormState extends State<TransactionForm> {
   final _titleController = TextEditingController();
   final _valueController = TextEditingController();
-  DateTime? _selectedDate;
+  DateTime _selectedDate = DateTime.now();
 
   _submitForm() {
     final title = _titleController.text;
     final value = double.tryParse(_valueController.text) ?? 0.0;
 
-    if(title.isEmpty || value <= 0) {
+    if(title.isEmpty || value <= 0 || _selectedDate == null) {
       return;
     }
 
-    widget.onSubmit!(title, value);
+    widget.onSubmit!(title, value, _selectedDate as DateTime);
   }
 
   _showDatePicker() {
