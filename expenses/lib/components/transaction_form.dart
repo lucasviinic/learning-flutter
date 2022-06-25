206a07460bc211be6c3b550977f38690ba4d5b81
lucasviinic <lvms@cin.ufpe.br>
2022-06-25 18:40:14 -0300

    refactor: ajustando formulário

--- expenses/lib/components/transaction_form.dart
+++ expenses/lib/components/transaction_form.dart
@@ -46,68 +46,76 @@ class _TransactionFormState extends State<TransactionForm> {
 
   @override
   Widget build(BuildContext context) {
-    return  Card(
-      elevation: 5,
-      child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          children: [
-            TextField(
-              controller: _titleController,
-              onSubmitted: (_) => _submitForm(),
-              decoration: InputDecoration(
-                labelText: "Título"
+    return  SingleChildScrollView(
+      padding: EdgeInsets.only(
+        top: 10,
+        left: 10,
+        right: 10,
+        bottom: 10 + MediaQuery.of(context).viewInsets.bottom
+      ),
+      child: Card(
+        elevation: 5,
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              TextField(
+                controller: _titleController,
+                onSubmitted: (_) => _submitForm(),
+                decoration: InputDecoration(
+                  labelText: "Título"
+                ),
               ),
-            ),
-            TextField(
-              controller: _valueController,
-              keyboardType: TextInputType.number,
-              onSubmitted: (_) => _submitForm(),
-              decoration: InputDecoration(
-                labelText: "Valor (R\$)"
+              TextField(
+                controller: _valueController,
+                keyboardType: TextInputType.number,
+                onSubmitted: (_) => _submitForm(),
+                decoration: InputDecoration(
+                  labelText: "Valor (R\$)"
+                ),
               ),
-            ),
-            Container(
-              height: 70,
-              child: Row(
+              Container(
+                height: 70,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        _selectedDate == null 
+                          ? "Nenhuma data selecionada"
+                          : "Data selecionada: ${DateFormat('dd/MM/y').format(_selectedDate as DateTime)}",
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: _showDatePicker, 
+                      child: Text(
+                        "Selecionar Data",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold
+                        ),
+                      )
+                    )
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  Expanded(
+                  ElevatedButton(
+                    onPressed: _submitForm, 
                     child: Text(
-                      _selectedDate == null 
-                        ? "Nenhuma data selecionada"
-                        : "Data selecionada: ${DateFormat('dd/MM/y').format(_selectedDate as DateTime)}",
+                      "Nova Transação", 
+                      style: TextStyle(
+                        color: Theme.of(context).textTheme.button?.color
+                      )
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Theme.of(context).colorScheme.primary,
                     ),
                   ),
-                  TextButton(
-                    onPressed: _showDatePicker, 
-                    child: Text(
-                      "Selecionar Data",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold
-                      ),
-                    )
-                  )
                 ],
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                ElevatedButton(
-                  onPressed: _submitForm, 
-                  child: Text(
-                    "Nova Transação", 
-                    style: TextStyle(
-                      color: Theme.of(context).textTheme.button?.color
-                    )
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    primary: Theme.of(context).colorScheme.primary,
-                  ),
-                ),
-              ],
-            )
-          ]
+              )
+            ]
+          ),
         ),
       ),
     );
